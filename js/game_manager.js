2e6f02912690f72aa0504765a3db648260482826
22shubh22 <22shubh22@gmail.com>
2019-01-17 19:19:55 +0530

    It's moving

--- js/game_manager.js
+++ js/game_manager.js
@@ -143,41 +143,18 @@ GameManager.prototype.move = function (direction) {
         var next      = self.grid.cellContent(positions.next);
 
         // Only one merger per row traversal?
-        if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
-          merged.mergedFrom = [tile, next];
+        
+      self.moveTile(tile, positions.farthest);
+        
 
-          self.grid.insertTile(merged);
-          self.grid.removeTile(tile);
-
-          // Converge the two tiles' positions
-          tile.updatePosition(positions.next);
-
-          // Update the score
-          self.score += merged.value;
-
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
-        } else {
-          self.moveTile(tile, positions.farthest);
-        }
-
-        if (!self.positionsEqual(cell, tile)) {
-          moved = true; // The tile moved from its original cell!
-        }
+      if (!self.positionsEqual(cell, tile)) {
+        moved = true; // The tile moved from its original cell!
       }
+    }
     });
   });
 
-  if (moved) {
-    this.addRandomTile();
-
-    if (!this.movesAvailable()) {
-      this.over = true; // Game over!
-    }
-
-    this.actuate();
-  }
+  this.actuate();
 };
 
 // Get the vector representing the chosen direction
